a895eba99d1f7089e0df16e8006b79bf9293aaf0
Eddie <eddie.ch.89d@gmail.com>
2021-02-22 15:32:20 +0100

    Student GUI

--- src/main/java/se/lexicon/gui/Gui.java
+++ src/main/java/se/lexicon/gui/Gui.java
@@ -3,6 +3,7 @@ package se.lexicon.gui;
 
 
 import javax.swing.*;
+import java.awt.*;
 
 
 
@@ -10,7 +11,16 @@ public class Gui {
 
     public Gui() {
         JFrame frame = new JFrame();
+
         JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(160, 160, 140, 140));
+        panel.setLayout(new GridLayout());
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle(" Student GUI ");
+        frame.pack();
+        frame.setVisible(true);
     }
 
     public static void main(String[] args) {
