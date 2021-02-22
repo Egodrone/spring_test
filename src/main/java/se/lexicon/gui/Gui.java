abe0a9b543242526533ae50636e6ce6938abe94f
Eddie <eddie.ch.89d@gmail.com>
2021-02-22 15:23:16 +0100

    GUI setup

---
+++ src/main/java/se/lexicon/gui/Gui.java
@@ -0,0 +1,19 @@
+package se.lexicon.gui;
+
+
+
+import javax.swing.*;
+
+
+
+public class Gui {
+
+    public Gui() {
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+    }
+
+    public static void main(String[] args) {
+        Gui gui = new Gui();
+    }
+}
