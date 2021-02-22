042d643e969174324783ed13f8f3b0ea3ac45b47
Eddie <eddie.ch.89d@gmail.com>
2021-02-22 12:06:17 +0100

    Junit test setup

--- src/test/java/se/lexicon/dao/StudentDaoTest.java
+++ src/test/java/se/lexicon/dao/StudentDaoTest.java
@@ -2,17 +2,39 @@ package se.lexicon.dao;
 
 
 
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import se.lexicon.config.AppConfig;
+import se.lexicon.model.Student;
 
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = {AppConfig.class})
 public class StudentDaoTest {
+
     @Autowired
-    StudentDao studentDao;
+    StudentDao testObject;
+
+
+
+    @Test
+    public void test_save_student() {
+        Student expectedResult = new Student("Test", 1);
+        Student actualResult = testObject.save(new Student("Test", 1));
+        Assert.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void test_delete() {
+        testObject.save(new Student("Test", 1));
+        testObject.save(new Student("Test2", 2));
+        testObject.delete(1);
+        //Assert.assertEquals();
+    }
+
 }
