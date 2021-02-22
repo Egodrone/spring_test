668a3493fbfe524c973a1b0ed668c5d4a9406ab4
Eddie <eddie.ch.89d@gmail.com>
2021-02-22 12:40:21 +0100

    StudentDaoJunit test

--- src/test/java/se/lexicon/dao/StudentDaoTest.java
+++ src/test/java/se/lexicon/dao/StudentDaoTest.java
@@ -3,6 +3,7 @@ package se.lexicon.dao;
 
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import se.lexicon.config.AppConfig;
 import se.lexicon.model.Student;
+import java.util.List;
 
 
 
@@ -22,6 +24,20 @@ public class StudentDaoTest {
 
 
 
+    @Before
+    public void setup() {
+
+        int a = 0;
+
+        while (a < 6) {
+            testObject.delete(a);
+            a += 1;
+        }
+
+    }
+
+
+
     @Test
     public void test_save_student() {
         Student expectedResult = new Student("Test", 1);
@@ -29,12 +45,39 @@ public class StudentDaoTest {
         Assert.assertEquals(expectedResult, actualResult);
     }
 
+
+
+    @Test
+    public void test_find_student_by_id() {
+        Student expectedResult = new Student("Test", 1);
+        testObject.save(new Student("Sven", 1));
+        testObject.save(new Student("Hanna", 2));
+        Student actualResult = testObject.save(new Student("Hanna", 3));
+        Student findById = testObject.find(3);
+        Assert.assertEquals(findById.getName(), actualResult.getName());
+    }
+
+
+
+    @Test
+    public void test_find_all_students() {
+        testObject.save(new Student("Sven", 1));
+        testObject.save(new Student("Hanna", 2));
+        testObject.save(new Student("Hanna", 3));
+        List<Student> studentList = testObject.findAll();
+        Assert.assertEquals(3, studentList.size());
+    }
+
+
+
     @Test
     public void test_delete() {
-        testObject.save(new Student("Test", 1));
-        testObject.save(new Student("Test2", 2));
+        testObject.save(new Student("Sven", 1));
+        testObject.save(new Student("Hanna", 2));
         testObject.delete(1);
-        //Assert.assertEquals();
+        List<Student> studentList = testObject.findAll();
+        studentList.forEach(System.out::println);
+        Assert.assertEquals(1, studentList.size());
     }
 
 }
