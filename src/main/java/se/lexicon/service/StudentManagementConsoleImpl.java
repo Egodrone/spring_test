abddaeb19db30770ceab4f08435cb3d1fb3fd8f7
Eddie <eddie.ch.89d@gmail.com>
2021-02-22 14:16:45 +0100

    StudentManagementConsoleImpl

--- src/main/java/se/lexicon/service/StudentManagementConsoleImpl.java
+++ src/main/java/se/lexicon/service/StudentManagementConsoleImpl.java
@@ -2,5 +2,42 @@ package se.lexicon.service;
 
 
 
-public class StudentManagementConsoleImpl {
+import se.lexicon.model.Student;
+import java.util.List;
+
+
+
+public class StudentManagementConsoleImpl implements StudentManagement{
+    @Override
+    public Student create() {
+        return null;
+    }
+
+    @Override
+    public Student save(Student student) {
+        return null;
+    }
+
+    @Override
+    public Student find(int id) {
+        return null;
+    }
+
+    @Override
+    public Student remove(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Student> findAll() {
+        return null;
+    }
+
+    @Override
+    public Student edit(Student student) {
+        return null;
+    }
+
+
+
 }
