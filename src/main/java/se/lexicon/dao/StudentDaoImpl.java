a22df8d1a3372fbde0343c1ffac1c83acf0b2662
Eddie <eddie.ch.89d@gmail.com>
2021-02-22 11:03:20 +0100

    Student model

--- src/main/java/se/lexicon/dao/StudentDaoImpl.java
+++ src/main/java/se/lexicon/dao/StudentDaoImpl.java
@@ -3,8 +3,7 @@ package se.lexicon.dao;
 
 
 public class StudentDaoImpl {
-    private String name;
-    private String lastname;
-    private int id;
+
+
 
 }
--- src/main/java/se/lexicon/dao/sequencer/Sequencer.java
+++ src/main/java/se/lexicon/dao/sequencer/Sequencer.java
@@ -3,6 +3,14 @@ package se.lexicon.dao.sequencer;
 
 
 public class Sequencer {
+    private static int studentSequencer;
 
+    public static int nextStudentUserId() {
+        return ++studentSequencer;
+    }
+
+    public static int getStudentUserIdSequencer() {
+        return studentSequencer;
+    }
 
 }
